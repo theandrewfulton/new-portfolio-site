2b82eb34ef86abfcd04613a89e34170e93c084c4
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-10-25 19:24:38 +1100

    add basic social media styling and flexbox

--- src/scenes/social/index.jsx
+++ src/scenes/social/index.jsx
@@ -1,5 +1,36 @@
+import styled from "styled-components"
+
+const StyledSocial = styled.section `
+    background: radial-gradient(100% 428429.78% at 100% -0.03%, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.2) 100%);
+    border: 0.5px solid rgba(255, 255, 255, 0.5);
+    backdrop-filter: blur(10px);
+    border: 0.5px solid rgba(255, 255, 255, 0.5);
+    border-radius: 15px;
+    margin: 20px;
+    padding: 10px;
+    display: flex;
+    justify-content: space-around;
+`
+const StyledIcon = styled.article `
+    background: radial-gradient(100% 100% at 0% 0%, rgba(255, 255, 255, 0.25) 0%, rgba(255, 255, 255, 0.15) 100%);
+    border: 0.5px solid rgba(255, 255, 255, 0.5);
+    backdrop-filter: blur(40px);
+    border-radius: 15px;
+    /* margin: 10px; */
+`
+
 export const Social = () => {
     return (
-        <p>This is the Social component</p>
+        <StyledSocial>
+            <StyledIcon>
+                <p>icon section</p>
+            </StyledIcon>
+            <StyledIcon>
+                <p>icon section</p>
+            </StyledIcon>
+            <StyledIcon>
+                <p>icon section</p>
+            </StyledIcon>
+        </StyledSocial>
     )
 }
