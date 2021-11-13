ad1b1d39786dc3a7754957cb69c81d0348ca3d88
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-11-13 16:19:39 +1100

    remove side borders for header and footer

--- src/scenes/nav/index.jsx
+++ src/scenes/nav/index.jsx
@@ -5,7 +5,8 @@ import styled from "styled-components"
 const DesktopNav = styled.nav`
 color: red;
 background: radial-gradient(100% 428429.78% at 100% -0.03%, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.2) 100%);
-border: 0.5px solid rgba(255, 255, 255, 0.5);
+border: 0.5px rgba(255, 255, 255, 0.5);
+border-style: solid none;
 backdrop-filter: blur(10px);
 /* clearfix hack to align right and stay in bounds */
 ::after {
