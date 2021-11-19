45045ff9555d536470519c75e3390d5b6052379b
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-11-19 22:48:30 +1100

    switch to navlinks to underline current page in nav

--- src/scenes/nav/index.jsx
+++ src/scenes/nav/index.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 export const NavContent = () => {
     return(
         <>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/projects">Projects</Link>
+            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+            <NavLink activeClassName="active" to="/about">About</NavLink>
+            <NavLink activeClassName="active" to="/projects">Projects</NavLink>
             <a href="https://theandrewfulton.medium.com" target="_blank" rel="noopener noreferrer">Articles</a>
-            <Link to="/contact">Contact</Link>
+            <NavLink activeClassName="active" to="/contact">Contact</NavLink>
         </>
     )
 }
