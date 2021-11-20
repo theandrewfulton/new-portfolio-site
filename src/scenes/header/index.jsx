b94d31cd2a35a1da226e6f6c108d49c2e59f89d6
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-11-20 23:15:23 +1100

    add logic for desktop vs mobile nav using hooks

--- src/scenes/header/index.jsx
+++ src/scenes/header/index.jsx
@@ -1,11 +1,46 @@
+import { useState, useEffect } from "react";
+
 import { DesktopHeader } from "./desktop"
 import { MobileHeader } from "./mobile"
-export const Header = () => {
 
+// adapted from https://usehooks.com/useWindowSize/
+// Hook
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+
+  return windowSize;
+}
+
+export const Header = () => {
+   const size = useWindowSize()    
     return (
         <>
-            <MobileHeader/>
-            <DesktopHeader/>
+        {size.width >= 950 ? <DesktopHeader/> : <MobileHeader/>}
         </>
     )
 }
