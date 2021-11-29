dfa0a537f21656d6f0c1a369a998db7d3a5fe0b8
Andrew Fulton <zenithsprings1@yahoo.com.au>
2021-11-29 18:22:30 +1100

    remove cache config and create a failing test

--- src/scenes/footer/footer.test.jsx
+++ src/scenes/footer/footer.test.jsx
@@ -6,3 +6,9 @@ test('renders text inside Footer component', () => {
   const footerText = screen.getByText('Copyright © 2021 Andrew Fulton');
   expect(footerText).toBeInTheDocument();
 });
+
+test('test github actions workflow', () => {
+  render(<Footer />);
+  const footerText = screen.getByText('Hello World');
+  expect(footerText).toBeInTheDocument();
+});
