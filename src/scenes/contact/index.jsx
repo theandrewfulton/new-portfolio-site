a702d6a91d4c3bc7aca21b8f821f95a12fa6c031
Andrew Fulton <zenithsprings1@yahoo.com.au>
2021-12-13 13:00:38 +1100

    move common page styling to new file

--- src/scenes/contact/index.jsx
+++ src/scenes/contact/index.jsx
@@ -3,31 +3,10 @@ import styled from "styled-components"
 import emailjs from 'emailjs-com'
 
 import { Social } from '../social'
+import { PageTransparency, PageContent} from '../styles'
 
 // import page contents
 import {ContactObject} from "./contactCopy"
-
-const TransparencySection = styled.section`
-background: radial-gradient(100% 241.89% at 100% 0%, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.2) 100%);
-border: 0.5px solid rgba(255, 255, 255, 0.5);
-backdrop-filter: blur(10px);
-
-/* Note: backdrop-filter has minimal browser support */
-border-radius: 15px;
-margin: 30px 5%;
-// width: 90vw;
-// padding: 5px 50px 50px 50px;
-padding: 5px 5% 50px 5%
-`
-
-const StyledIcon = styled.article `
-    background: radial-gradient(100% 100% at 0% 0%, rgba(255, 255, 255, 0.25) 0%, rgba(255, 255, 255, 0.15) 100%);
-    border: 0.5px solid rgba(255, 255, 255, 0.5);
-    backdrop-filter: blur(40px);
-    border-radius: 15px;
-    margin: auto;
-    padding: 4%;
-`
 
 const FormStyling = styled.article`
 
@@ -240,11 +219,11 @@ const ContactForm = ({description}) => {
 export const Contact = () => {
     
     return (
-        <TransparencySection>
+        <PageTransparency>
             <h1>Contact Me</h1>
-            <StyledIcon>
+            <PageContent>
                 <ContactForm {...ContactObject}/>               
-            </StyledIcon>
-        </TransparencySection>
+            </PageContent>
+        </PageTransparency>
     )
 }
---
+++ src/scenes/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+// Could this move to App in future and pass title and content as params?
+
+// Styling for semi-transparent box around page content
+export const PageTransparency = styled.section`
+background: radial-gradient(100% 241.89% at 100% 0%, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.2) 100%);
+border: 0.5px solid rgba(255, 255, 255, 0.5);
+backdrop-filter: blur(10px);
+/* Note: backdrop-filter has minimal browser support */
+border-radius: 15px;
+margin: 30px 5%;
+padding: 5px 5% 50px 5%;
+`
+// Styling for page content
+export const PageContent = styled.article `
+    background: radial-gradient(100% 100% at 0% 0%, rgba(255, 255, 255, 0.25) 0%, rgba(255, 255, 255, 0.15) 100%);
+    border: 0.5px solid rgba(255, 255, 255, 0.5);
+    backdrop-filter: blur(40px);
+    border-radius: 15px;
+    margin: auto;
+    padding: 4%;
+`
