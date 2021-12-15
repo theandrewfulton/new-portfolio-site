0e0bdab5100c43e9713bcc3ae2b27b5c5d213372
Andrew Fulton <zenithsprings1@yahoo.com.au>
2021-12-15 22:09:04 +1100

    add browser time-based greeting

--- src/scenes/home/index.jsx
+++ src/scenes/home/index.jsx
@@ -23,12 +23,37 @@ const PageContentExtended = styled(PageContent) `
     }
 `
 
+// greet user based on time
+const timeGreeting = () => {
+    const currentTime = new Date()
+    const timeConvert = currentTime.getHours()
+    console.log(timeConvert)
+
+    if (timeConvert < "12" ) {
+        return (
+            "Good Morning"
+        )
+    } else if (timeConvert >= "12" && timeConvert < "18") {
+        return (
+            "Good Afternoon"
+        )
+    } else if (timeConvert >= "18" && timeConvert < "24") {
+        return (
+            "Good Evening"
+        )
+    } else {
+        return (
+            "Hello"
+        )
+    }
+}
+
 export const Home = () => {
     return (
         <PageTransparency>
             <PageContentExtended>
                 {/* <p>This is the Home component</p> */}
-                <h1>Hello</h1> {/* greeting based on browser time? */}
+                <h1>{timeGreeting()}</h1>
                 <h2>My name is Andrew Fulton</h2>
                 <h3>I'm a Full-Stack Developer based in Sydney, Australia</h3>
                 <p>I also have Consulting, Mentoring and Startup experience</p>
